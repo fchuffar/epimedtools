1ae06ae4c032a5a17e91c09028dfda711984b261
Florent Chuffart <florent.chuffart@gmail.com>
2016-02-11 16:32:53 +0100

    Updation raw data path for Study_raw_trscr test and vignette

--- tests/testthat/test-Study_raw_trscr.r
+++ tests/testthat/test-Study_raw_trscr.r
@@ -2,18 +2,24 @@ context("Study_raw_trscr")
 
 test_that("Study_loc objects could be plot", {
   study = create_study(Study_RC_name="Study_raw_trscr")
-  kc_cel_filedir = system.file(
-    "extdata/trscr_raw_kc", 
-    package = "epimedtools"
-  )
-  ctrl_cel_filedir = system.file(
-    "extdata/trscr_raw_ctrl", 
-    package = "epimedtools"
-  )
+  print("**************************")
+  print(getwd())
+  # kc_cel_filedir = system.file(
+  #   "extdata/trscr_raw_kc",
+  #   package = "epimedtools"
+  # )
+  # ctrl_cel_filedir = system.file(
+  #   "extdata/trscr_raw_ctrl",
+  #   package = "epimedtools"
+  # )
+  kc_cel_filedir = "../../inst/extdata/trscr_raw_kc"
+  ctrl_cel_filedir = "../../inst/extdata/trscr_raw_ctrl"
   study$cel_filedirs = c(kc_cel_filedir, ctrl_cel_filedir)
-  data = study$get_data(celfile.path="")
+  data = study$get_data()
   exp_grp = study$get_exp_grp()
+  ratio = study$get_ratio()
   expect_equal(dim(data), c(54675,6))
+  expect_equal(dim(ratio), c(54675,6))
   expect_equal(dim(exp_grp), c(6,1))
 })
 
