871d5cb73cd5624f2244358eca6f413c1a47cd6c
Florent Chuffart <florent.chuffart@gmail.com>
2018-10-24 16:53:20 +0200

    Move get_transcriptogrm feature to deeptoolsr package

--- R/utils.R
+++ R/utils.R
@@ -324,49 +324,6 @@ tcga_merge_metadata_cart_and_clinicals = function(metadata_json_file, clinical_j
 
 
 
-#' A Function That Computes Transcriptogram of a Gene.
-#'
-#' This function computes  RNA-Seq signal matrix of a gene from coverage signal.
-#' @param exons_bed a bed-like dataframe of exons.
-#' @param coverage_bwfiles a vector of bigwig files.
-#' @param bin_size an integer specifying the bin to discretize RNA-Seq signal.
-#' @param tmp_dir a string specifying temporary directory.
-#' @export
-#' @importFrom utils write.table
-get_transcriptogram = function(exons_bed, coverage_bwfiles, bin_size=5, tmp_dir="tmp") {
-  bar = lapply(1:nrow(exons_bed), function(i) {
-    before_region_start_length = 0
-    after_region_start_length = exons_bed[i,3] - exons_bed[i,2]
-    nb_bin = ceiling((before_region_start_length + after_region_start_length) / bin_size)
-    after_region_start_length = nb_bin * bin_size
-    dir.create(tmp_dir, recursive=TRUE, showWarnings=FALSE)
-    matrix_out_filename = paste0(tmp_dir, "/matrix_", i, "_RNA.txt.gz")
-    if (!file.exists(matrix_out_filename)) {
-      regions_filename = paste0("tmp/exons_bed_", i, ".bed")
-      write.table(exons_bed[i,1:6], file=regions_filename, sep="\t", quote=FALSE,row.names=FALSE, col.names=FALSE)      
-      out_filename = matrix_out_filename
-      matrix_out_filename = dt_compute_matrix(regions_filename=regions_filename, score_filename=coverage_bwfiles, out_filename=out_filename, bin_size=bin_size, before_region_start_length=before_region_start_length, after_region_start_length=after_region_start_length, FORCE_EXEC=TRUE)
-      # matrix_file = matrix_out_filename
-      # out_filename = "hm.png"
-      # dt_plot_heatmap(matrix_file, out_filename)    
-    }
-
-    foo = read.table(gzfile(matrix_out_filename), skip=1, stringsAsFactors=FALSE)
-    # head(foo)
-    # print(dim(foo))
-    data = matrix(as.numeric(unlist(t(foo[,-(1:6)]))), nb_bin*nrow(foo))
-    rownames(data) = paste0("exon_", rep(i, nb_bin), "_", 1:nb_bin)
-    colnames(data) = do.call(rbind, strsplit(do.call(rbind,lapply(strsplit(coverage_bwfiles, "/"), rev))[,1], "_"))[,1]
-  
-    return(data)
-  })
-  bar = do.call(rbind,bar)
-  return(bar)  
-}
-
-
-
-
 #' A Function That Perform Intersection Between Sets.
 #'
 #'
