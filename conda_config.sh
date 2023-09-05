dd8d62d4e20cbdc39dabbefa34b6465d7782f106
Florent Chuffart <florent.chuffart@gmail.com>
2023-09-05 16:03:31 +0200

    Update conda_config.sh

--- conda_config.sh
+++ conda_config.sh
@@ -6,6 +6,7 @@ conda install -c conda-forge r-base=4.2.0
 conda install -c bioconda bioconductor-geoquery bioconductor-affy  bioconductor-biobase r-seqinr
 conda install -c r r-rcpparmadillo 
 conda install -c conda-forge r-devtools
+conda install -c conda-forge r-fastmap=1.1.1
 
 # under R
 # devtools::install_github("fchuffar/epimedtools")
