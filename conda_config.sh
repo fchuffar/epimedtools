4c66b0567ff1909da742f1742f86526268b09b5d
Florent Chuffart <florent.chuffart@gmail.com>
2023-10-24 22:24:19 +0200

    Update conda_config.sh

--- conda_config.sh
+++ conda_config.sh
@@ -1,12 +1,9 @@
 conda create --name epimedtools_env
 conda activate epimedtools_env
 
-conda install  -c anaconda libopenblas
-conda install -c conda-forge r-base=4.2.0
-conda install -c bioconda bioconductor-geoquery bioconductor-affy  bioconductor-biobase r-seqinr
-conda install -c r r-rcpparmadillo 
-conda install -c conda-forge r-devtools
-conda install -c conda-forge r-fastmap=1.1.1
+
+conda install -n base conda-forge::mamba
+mamba install -c anaconda -c bioconda -c conda-forge -c r libopenblas=0.3.21  r-base=4.2.0  bioconductor-geoquery=2.66.0  bioconductor-affy=1.76.0  bioconductor-biobase=2.58.0  r-seqinr=4.2_30  r-rcpparmadillo=0.12.6.4.0  r-devtools=2.4.5  r-fastmap=1.1.1 libstdcxx-devel_linux-64=11.2.0
 
 # under R
 # devtools::install_github("fchuffar/epimedtools")
